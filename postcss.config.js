4c0106bbb91fe38fa8bbbc853e82c39eec7abcd0
Abid Hasan kiam <kiamhasan267@gmail.com>
2024-07-14 21:56:49 +0600

    fix : seo

--- postcss.config.js
+++ postcss.config.js
@@ -1,29 +1,37 @@
+const path = require('path');
+
+
 module.exports = {
-  plugins: [
-    'postcss-flexbugs-fixes',
-    [
-      'postcss-preset-env',
-      {
-        autoprefixer: {
-          flexbox: 'no-2009',
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  postcss: {
+    plugins: [
+      'postcss-flexbugs-fixes',
+      [
+        'postcss-preset-env',
+        {
+          autoprefixer: {
+            flexbox: 'no-2009',
+          },
+          stage: 3,
+          features: {
+            'custom-properties': false,
+          },
         },
-        stage: 3,
-        features: {
-          'custom-properties': false,
+      ],
+      [
+        '@fullhuman/postcss-purgecss',
+        {
+          content: [
+            './pages/**/*.{js,jsx,ts,tsx}',
+            './components/**/*.{js,jsx,ts,tsx}',
+          ],
+          defaultExtractor: content =>
+            content.match(/[\w-/:]+(?<!:)/g) || [],
+          safelist: ['html', 'body'],
         },
-      },
+      ],
     ],
-    [
-      '@fullhuman/postcss-purgecss',
-      {
-        content: [
-          './pages/**/*.{js,jsx,ts,tsx}',
-          './components/**/*.{js,jsx,ts,tsx}',
-        ],
-        defaultExtractor: content =>
-          content.match(/[\w-/:]+(?<!:)/g) || [],
-        safelist: ['html', 'body'],
-      },
-    ],
-  ],
+  },
 };
